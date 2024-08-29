b8e5a524f6bb442aa8c291db9b30449e91ae12b3
GabrielDittrich <gdittrchcj@gmail.com>
2024-08-29 11:12:46 -0300

    Fazendo Requisições

--- Aula02/MinhaSolucao/PrimeiraMinimalAPI/Program.cs
+++ Aula02/MinhaSolucao/PrimeiraMinimalAPI/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+// Minimal APIs
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Endpoints -- Funcionalidades
+// Request   -- Requisição -- URL e o método/verbo HTTP
+app.MapGet("/", () => "Hello, Minimal API!");
+
+// ... outros endpoints
+app.MapGet("/items", () => "Obter todos os itens");
+app.MapPost("/items", () => "Adicionar um novo item");
+
+app.MapGet("/items/{id}", (int id) => $"Obter item com ID {id}");
+
 
 app.Run();
