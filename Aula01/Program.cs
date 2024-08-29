e9c6ed977125a56cbc540d205427d1af470aab16
GabrielDittrich <gdittrchcj@gmail.com>
2024-08-29 09:27:32 -0300

    Começando CSharp

---
+++ Aula01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Quantos anos você tem?");
+String idade = Console.ReadLine();
+
+Console.WriteLine("Você tem : " + idade + " Anos de idade");
