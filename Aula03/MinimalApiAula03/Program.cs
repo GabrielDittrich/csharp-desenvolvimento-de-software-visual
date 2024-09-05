b349847cc85a213a2e77716ea2fdea2352400444
GabrielDittrich <gdittrchcj@gmail.com>
2024-09-05 11:21:15 -0300

    Vendo Listas / new List<>

--- Aula03/MinimalApiAula03/Program.cs
+++ Aula03/MinimalApiAula03/Program.cs
@@ -1,27 +1,55 @@
 
 // Minimal APIs
+using MinimalApiAula03.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//Lista de Produtos
+List<Produto> produtos = new List<Produto>
+{
+    new Produto
+    {
+        Nome = "Produto A",
+        Preco = 19.99,
+        Quantidade = 10,
+    },
+    new Produto
+    {
+        Nome = "Produto B",
+        Preco = 29.99,
+        Quantidade = 5,
+    },
+    new Produto
+    {
+        Id = "3",
+        Nome = "Produto C",
+        Preco = 9.99,
+        Quantidade = 25,
+    }
+};
+
 // Endpoints -- Funcionalidades
 // Request   -- Requisição -- URL e o método/verbo HTTP
-app.MapGet("/", () => "Hello, Minimal API!");
+app.MapGet("/", () => "API de Produtos!");
 
-// ... outros endpoints
-app.MapGet("/items", () => "Obter todos os itens");
-app.MapPost("/items", () => "Adicionar um novo item");
-
-app.MapGet("/items/{id}", (int id) => $"Obter item com ID {id}");
-
-//  Endpoint Dinamico
-app.MapGet("/retornarendereco", () =>
+// GET:  http://localhost:5163/produto/listar
+app.MapGet("/produto/listar", () =>
 {
-    dynamic endereco = new
-    {
-        rea = "Praça Osório",
-        numero = 125
-    };
-    return endereco;
+    return produtos;
 });
 
+app.MapPost("/produtos/cadastrar", () =>
+{
+    return produtos;
+});
+
+/* Em java
+Produto produto = new Produto();
+produto.setPreco(8800);
+System.out.println("O produto custa "+ produto.getPreco()); */
+
+Produto produto = new Produto();
+produto.Preco = 777;
+Console.WriteLine("Preço: " + produto.Preco);
 app.Run();
