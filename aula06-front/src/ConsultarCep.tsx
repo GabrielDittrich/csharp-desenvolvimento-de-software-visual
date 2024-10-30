43075a25d1a66a2d11de242f5fcd42a6412403c1
Gabriel Dittrich <gdittrchcj@gmail.com>
2024-10-30 18:08:24 -0300

    Começando FrontEnd com React

--- aula06-front/src/ConsultarCep.tsx
+++ aula06-front/src/ConsultarCep.tsx
@@ -1,23 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function ConsultarCep(){
     
+    const [cepData, setCepData] = useState<any>(null);
+
     useEffect(() => {
         // Método utilizado para executar qualquer codigo enquanto
         //o componente está sendo aberto/renderizado
         //Biblioteca de requisições - AXIOS
-        fetch("https://viacep.com.br/ws/01001000/json/")
+
+
+        fetch("https://viacep.com.br/ws/80020010/json/")
             .then(resposta => {
                 return resposta.json();
             })
-            .then(cep => {
-                console.log(cep);
+            .then((data) => {
+                // Armazenando os dados da resposta no estado
+                setCepData(data);
             });
     });
 
     return(
         <div>
             <h1>Consultar CEP</h1>
+            {cepData? (
+                <div>
+                    <p><strong>CEP:</strong> {cepData.cep}</p>
+                    <p><strong>Logradouro:</strong> {cepData.logradouro}</p>
+                    <p><strong>Bairro:</strong> {cepData.bairro}</p>
+                    <p><strong>Cidade:</strong> {cepData.localidade}</p>
+                    <p><strong>Estado:</strong> {cepData.uf}</p>
+                </div>
+            ) : (
+                <p>Carregando...</p>
+            )}
         </div>
     );
 }
