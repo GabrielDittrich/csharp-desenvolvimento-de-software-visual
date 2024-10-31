1c2409e88fedcd76e4e044e7a054517266e289c9
Gabriel Dittrich <gdittrchcj@gmail.com>
2024-10-31 15:53:57 -0300

    Começando Frontend com React
    Ajeitando o Backend para a próxima aula de React adicionando o modelo Categoria

--- Aula06/Back/Models/AppDataContext.cs
+++ Aula06/Back/Models/AppDataContext.cs
@@ -6,7 +6,8 @@ namespace Back.Models;
 public class AppDataContext : DbContext
 {
     public DbSet<Produto> Produtos { get; set; }
-
+     public DbSet<Categoria> Categorias { get; set; }
+     
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=Ecommerce.db");
