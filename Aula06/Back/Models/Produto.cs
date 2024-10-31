62e1d2c3ca58d12028700a03857bd0f23dae329f
Gabriel Dittrich <gdittrchcj@gmail.com>
2024-10-31 15:50:24 -0300

    Começando Frontend com React
    Ajeitando o Backend para a próxima aula de React

---
+++ Aula06/Back/Models/Produto.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Back.Models;
+
+public class Produto
+{
+    public Produto()
+    {
+        Id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+    }
+
+    public string? Id { get; set; }
+
+    public string? Nome { get; set; }
+    
+    public string? Descricao { get; set; }
+
+
+    public double Preco { get; set; }
+
+    public int Quantidade { get; set; }
+
+    public DateTime CriadoEm { get; set; }
+
+}
